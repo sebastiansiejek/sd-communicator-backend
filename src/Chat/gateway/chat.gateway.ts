15767679d94736c9cd72c5a519851be3fdc9892e
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-11-14 13:54:27 +0100

    feat: add sample chat gateway

---
+++ src/Chat/gateway/chat.gateway.ts
@@ -0,0 +1,33 @@
+import { Logger } from '@nestjs/common'
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer
+} from '@nestjs/websockets'
+import { Server, Socket } from 'socket.io'
+
+@WebSocketGateway()
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() wss: Server
+
+  private logger: Logger = new Logger('ChatGateway')
+
+  afterInit() {
+    this.logger.log('Initialized')
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`)
+  }
+
+  handleConnection(client: Socket) {
+    this.logger.log(`Client connected: ${client.id}`)
+  }
+
+  @SubscribeMessage('msgToServer')
+  handleMessage(client: Socket, text: string): void {
+    this.wss.emit('msgToClient', text)
+  }
+}
