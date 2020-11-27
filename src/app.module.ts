b324bbcc2661393abd95337a8c2d9a77e2d74202
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-11-27 21:46:32 +0100

    chore: remove default app modules, controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { ChatGateway } from './chat/gateway/chat.gateway'
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService, ChatGateway]
+  controllers: [],
+  providers: [ChatGateway]
 })
 export class AppModule {}
