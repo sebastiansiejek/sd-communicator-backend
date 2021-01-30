574eeae6a2313c9707d60c07404cdf7d6e932a2d
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-01-30 18:30:33 +0100

    feat: set allow requests to WEBSOCKET_CLIENT_ORIGIN

--- src/Adapters/AuthenticateWsIoAdapter.ts
+++ src/Adapters/AuthenticateWsIoAdapter.ts
@@ -3,7 +3,9 @@ import { IoAdapter } from '@nestjs/platform-socket.io'
 export class AuthenticatedWsIoAdapter extends IoAdapter {
   createIOServer(port: number, options?: any): any {
     options.allowRequest = async (request: any, allowFunction) => {
-      return allowFunction(null, true)
+      if (request.headers.origin === process.env.WEBSOCKET_CLIENT_ORIGIN)
+        return allowFunction(null, true)
+      else return false
     }
     return super.createIOServer(port, options)
   }
